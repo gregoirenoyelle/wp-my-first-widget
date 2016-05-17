d1e46e34a8bc69bbab89ce8d57203fc6d3c51282
Grégoire Noyelle <hello@gregoirenoyelle.com>
2016-05-17 15:14:18 +0200

    Filtrage des données dans l'affichage des données dans le widget

--- views/admin.php
+++ views/admin.php
@@ -1,4 +1,4 @@
 <div class="my-first-widget">
 	<label><?php _e('Your Name', 'my-first-widget'); ?></label>
-	<input type="text" class="widefat" id="<?php echo $this->get_field_id('name'); ?>" name="<?php echo $this->get_field_name('name'); ?>" value="<?php echo $instance['name']; ?>" />
+	<input type="text" class="widefat" id="<?php echo $this->get_field_id('name'); ?>" name="<?php echo $this->get_field_name('name'); ?>" value="<?php echo esc_attr( $instance['name'] ); ?>" />
 </div>
