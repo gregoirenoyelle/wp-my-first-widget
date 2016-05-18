6872b97c29b81159a3d76cc314270f60e4eb735f
Grégoire Noyelle <hello@gregoirenoyelle.com>
2016-05-18 06:32:10 +0200

    Intégration du champ bio dans le formulaire du widget

--- views/admin.php
+++ views/admin.php
@@ -1,4 +1,13 @@
 <div class="my-first-widget">
-	<label><?php _e('Your Name', 'my-first-widget'); ?></label>
-	<input type="text" class="widefat" id="<?php echo $this->get_field_id('name'); ?>" name="<?php echo $this->get_field_name('name'); ?>" value="<?php echo esc_attr( $instance['name'] ); ?>" />
+
+	<div>
+		<label><?php _e('Your Name', 'my-first-widget'); ?></label>
+		<input type="text" class="widefat" id="<?php echo $this->get_field_id('name'); ?>" name="<?php echo $this->get_field_name('name'); ?>" value="<?php echo esc_attr( $instance['name'] ); ?>" />
+	</div>
+
+	<div>
+		<label><?php _e('Your bio', 'my-first-widget'); ?></label>
+		<textarea id="<?php echo $this->get_field_id('bio'); ?>" class="widefat" name="<?php echo $this->get_field_name('bio'); ?>" rows="3" cols="30"><?php echo esc_textarea( $instance['bio'] ); ?></textarea>
+	</div>
+
 </div>
